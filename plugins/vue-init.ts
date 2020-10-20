594ab2cfd3ff4b4b57a89ddabb0d74e2fb095b35
_uchanlee <uchanlee.dev@gmail.com>
2020-10-20 12:09:14 +0900

    feat: create plugin vue-init.ts

---
+++ plugins/vue-init.ts
@@ -0,0 +1,13 @@
+// import { Context } from '@nuxt/types';
+
+// export default function (context: Context) {
+//   console.log(context);
+//   console.log(context.store.state);
+//   console.log(context.store.$router);
+//   localStorage.setItem('accessToken', 'asd.asd.asd');
+//   const accessToken = localStorage.getItem('accessToken');
+//   if (accessToken) {
+//     console.log(accessToken);
+//     return context.redirect(200, '/login');
+//   }
+// }
