0c2e77199673f4157081f2475406708ac45cb3ea
_uchanlee <uchanlee.dev@gmail.com>
2020-10-21 19:54:48 +0900

    feat: create report-list route

---
+++ pages/report-list/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <report-list />
+</template>
+
+<script>
+import ReportList from '~/components/ReportList';
+
+export default {
+  name: 'ReportList',
+  layout: 'loggedIn',
+  components: {
+    'report-list': ReportList,
+  },
+};
+</script>
