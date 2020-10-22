b82bf3e8a26b5bf4b1b16c2c070508bc8f4d1950
_uchanlee <uchanlee.dev@gmail.com>
2020-10-22 11:35:02 +0900

    refactor: fix ts to js

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
   css: [{ src: '~assets/styles/main.scss', lang: 'scss' }],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [{ src: '~/plugins/vue-init.ts', mode: 'client' }],
+  plugins: [{ src: '~/plugins/vue-init.js', mode: 'client' }],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
