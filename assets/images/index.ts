d7e4126acc4bd2132eeb544296976095fc6d79a7
_uchanlee <uchanlee.dev@gmail.com>
2020-10-22 10:13:34 +0900

    feat: add export image

--- assets/images/index.ts
+++ assets/images/index.ts
@@ -3,3 +3,4 @@ export { default as frameLogo } from './frame-logo.svg';
 export { default as warningImage } from './warning-image.svg';
 export { default as warningImpactImage } from './warning-impact-image.svg';
 export { default as pencilImage } from './pencil-image.svg';
+export { default as closeImage } from './close-image.svg';
