ca6b9ff70c1b2ad713c2e98a9ffdd94b0ae0cf69
_uchanlee <uchanlee.dev@gmail.com>
2020-11-22 03:37:08 +0900

    feat: #45 create notice mutations

---
+++ store/notice/mutations.js
@@ -0,0 +1,15 @@
+import { initialState } from './state';
+
+export const CREATE_NOTICE_SUCCESS = 'CREATE_NOTICE_SUCCESS';
+export const RESET_STATE = 'RESET_STATE';
+
+export const resetStateMutation = () => `notice/${RESET_STATE}`;
+
+export default {
+  [CREATE_NOTICE_SUCCESS]: function (state) {
+    state.isSuccessCreateNotice = true;
+  },
+  [RESET_STATE]: function (state) {
+    state = initialState;
+  },
+};
