4f94f31e804e617c12a4ddad786d07c39e6afdd9
_uchanlee <uchanlee.dev@gmail.com>
2020-11-22 20:41:20 +0900

    style: lint

--- utils/pagination/index.js
+++ utils/pagination/index.js
@@ -3,4 +3,4 @@ export const getTotalPage = (total, unit) => Math.ceil(total / unit);
 export const getRangedArray = (array, currentPage, unit) => {
   const startIndex = (currentPage - 1) * unit;
   return array.slice(startIndex, startIndex + unit);
-}
+};
