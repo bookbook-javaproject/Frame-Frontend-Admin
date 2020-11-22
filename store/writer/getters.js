242a15e72a102b5314ec9e90c4d02be04685bca7
_uchanlee <uchanlee.dev@gmail.com>
2020-11-22 16:12:45 +0900

    style: lint

--- store/writer/getters.js
+++ store/writer/getters.js
@@ -3,15 +3,15 @@ const PAGE_UNIT = 3;
 export const TOTAL_PAGE = 'TOTAL_PAGE';
 export const REANGED_WRITERS = 'REANGED_WRITERS';
 
-export const totalPageGetter = () => `writer/${TOTAL_PAGE}`
-export const rangedWritersGetter = () => `writer/${REANGED_WRITERS}`
+export const totalPageGetter = () => `writer/${TOTAL_PAGE}`;
+export const rangedWritersGetter = () => `writer/${REANGED_WRITERS}`;
 
 export default {
   [TOTAL_PAGE](state) {
     return Math.ceil(state.writers.length / PAGE_UNIT);
   },
-  [REANGED_WRITERS] (state) {
+  [REANGED_WRITERS](state) {
     const startIndex = (state.currentPage - 1) * PAGE_UNIT;
     return state.writers.slice(startIndex, startIndex + PAGE_UNIT);
-  }
+  },
 };
